cbb79acf0e268782c096a9a8c50a140492472bc1
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2023-02-20 02:45:07 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+# Telegram Bot API token
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+
+# OpenAI API key
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+# MongoDB connection URI
+MONGODB_URI = os.environ.get('MONGODB_URI')
