90bb8a751eda846456cddc8c4cc0912634259a84
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2023-02-20 03:20:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,36 @@
-import telebot
-import random
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import TELEGRAM_TOKEN, TELEGRAM_APP_NAME
+from command import logo_handler, start_handler, help_handler, dc_handler, status_handler, broadcast_handler, about_handler
 
-# Your bot's access token provided by BotFather
-TOKEN = 'your-bot-token-here'
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Create an instance of the bot
-bot = telebot.TeleBot(TOKEN)
+def main():
+    # Create the Updater and pass it your bot's token
+    updater = Updater(TELEGRAM_TOKEN, use_context=True)
 
-# Define a function to generate a logo
-def generate_logo(request):
-    # your code to generate the logo
-    # return the logo file path
-    return "path-to-generated-logo-file"
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
 
-# Define a handler for the /logo command
-@bot.message_handler(commands=['logo'])
-def handle_logo_command(message):
-    # Extract the user's request from the message
-    request = message.text[6:]
+    # Register handlers
+    dp.add_handler(CommandHandler("logo", logo_handler))
+    dp.add_handler(CommandHandler("start", start_handler))
+    dp.add_handler(CommandHandler("help", help_handler))
+    dp.add_handler(CommandHandler("dc", dc_handler))
+    dp.add_handler(CommandHandler("status", status_handler))
+    dp.add_handler(CommandHandler("broadcast", broadcast_handler))
+    dp.add_handler(CommandHandler("about", about_handler))
 
-    # Generate the logo using the request
-    logo_file_path = generate_logo(request)
+    # Start the Bot
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TELEGRAM_TOKEN)
+    updater.bot.setWebhook('https://{}.herokuapp.com/{}'.format(TELEGRAM_APP_NAME, TELEGRAM_TOKEN))
 
-    # Send the logo file to the user
-    with open(logo_file_path, 'rb') as logo_file:
-        bot.send_photo(message.chat.id, logo_file)
+    # Run the bot until you press Ctrl-C or the process receives SIGINT, SIGTERM or SIGABRT
+    updater.idle()
 
-# Start the bot
-bot.polling()
+if __name__ == '__main__':
+    main()
