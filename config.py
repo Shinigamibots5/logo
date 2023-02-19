958ba9a759fad5468aa3bb4cbf6887459e3ec676
Shinigamibots5 <100681700+Shinigamibots5@users.noreply.github.com>
2023-02-20 04:04:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 
 # OpenAI API key
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+OPENAI_API_KEY = os.environ.get('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')
 
 # MongoDB connection URI
 MONGODB_URI = os.environ.get('MONGODB_URI')
